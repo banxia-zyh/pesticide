bd5422afa66ecb6b964338a3d2a6057f87567adf
Geolo <geolo1997@163.com>
2019-10-07 16:27:57 +0800

    创建项目

---
+++ src/main/java/group/geolo/defectmanager/DefectManagerServerApplication.java
@@ -0,0 +1,13 @@
+package group.geolo.defectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefectManagerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefectManagerServerApplication.class, args);
+    }
+
+}
